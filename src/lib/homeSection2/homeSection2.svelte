e40bcfff39cb1cd3ecd3022e64c04b8aba29bfee
sly <sylvester@archis.company>
2024-10-16 22:32:04 +0100

    implement homeSection2

--- src/lib/homeSection2/homeSection2.svelte
+++ src/lib/homeSection2/homeSection2.svelte
@@ -1,7 +1,43 @@
 <script lang="ts">
-	import SectionWrapper from "$lib/sectionWrapper/sectionWrapper.svelte"
+	import styles from "./homeSection2.module.scss"
+
+	const options = [
+		{
+			icon: "lightbulb_2",
+			title: "innovative solutions for sustanable development in cameroon",
+			description:
+				"We focus on enhancing the socio-economic conditions of orphans through targeted initiatives."
+		},
+		{
+			icon: "school",
+			title: "Transforming Lives Through Education and Technology",
+			description: "Our IT education programs equip orphans with essential skills for the future."
+		},
+
+		{
+			icon: "water_bottle",
+			title: "Providing Clean Water for Healthier Communities",
+			description: "We implement potable water projects to ensure access to clean drinking water."
+		}
+	]
 </script>
 
-<SectionWrapper>
-	<h1>Hello</h1>
-</SectionWrapper>
+<section class={styles.homeSection2}>
+	<div class={styles.sectionWrapper}>
+		<h2 class={styles.contentTitle}>
+			empowering
+			<span class={styles.highlight}>orphans</span> and vulnerable communities
+			<span class={styles.highlight}>for a brighter future</span>
+		</h2>
+
+		<div class={styles.sectionContent}>
+			{#each options as { icon, title, description }}
+				<article>
+					<span class="material-symbols-sharp">{icon}</span>
+					<h4>{title}</h4>
+					<p>{description}</p>
+				</article>
+			{/each}
+		</div>
+	</div>
+</section>
