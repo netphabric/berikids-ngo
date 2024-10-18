6f36ac57ce81e66e641a12f861a945ec079854b3
sly <sylvester@archis.company>
2024-10-18 07:59:58 +0100

    swap out cloudflare adapter for netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
+import adapter from "@sveltejs/adapter-netlify"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
-import adapter from "@sveltejs/adapter-cloudflare"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,10 +7,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			routes: {
-				include: ["/*"],
-				exclude: ["<all>"]
-			}
+			edge: false,
+			split: false
 		}),
 		alias: {
 			"styles/*": "./src/styles/*",
