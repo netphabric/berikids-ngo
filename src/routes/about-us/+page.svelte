641fdcae8e163afbf81638d8849afd51cd2fb848
sly <sylvester@archis.company>
2024-10-23 10:39:54 +0100

    implement sectors section

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -2,6 +2,24 @@
 	import Button from "$lib/components/button/button.svelte"
 	import SectionWrapper from "$lib/components/sectionWrapper/sectionWrapper.svelte"
 	import styles from "./page.module.scss"
+
+	const sectors = [
+		{
+			image: "/images/s1.webp",
+			title: "Enhancing Socio-Economic Conditions for Orphans",
+			desc: "We provide vocational training to empower orphans with essential life skills."
+		},
+		{
+			image: "/images/s2.webp",
+			title: "Advancing Education and Technology in Cameroon",
+			desc: "Our programs promote digital literacy as a key to development."
+		},
+		{
+			image: "/images/s3.webp",
+			title: "Providing Clean Water to Communities in Need",
+			desc: "We ensure access to potable water for underserved populations."
+		}
+	]
 </script>
 
 <main class={styles.aboutRoot}>
@@ -94,4 +112,49 @@
 			<img src="/images/as2.webp" alt="about-us" />
 		</div>
 	</SectionWrapper>
+
+	<section class={styles.sectorSection}>
+		<div class={styles.sectorHeader}>
+			<h4 class={styles.chip}>
+				<span class="material-symbols-sharp">potted_plant</span>
+				<span>development</span>
+			</h4>
+
+			<h2 class={styles.title}>
+				our commitment to community
+				<span class={styles.highlight}>development</span>
+			</h2>
+
+			<p class={styles.desc}>
+				We strive to uplift the lives of orphans and vulnerable communities in Cameroon. Our
+				comprehensive approach combines education, health, and spiritual guidance to foster
+				sustainable growth.
+			</p>
+		</div>
+
+		<div class={styles.sectors}>
+			{#each sectors as sector}
+				<article class={styles.sectorCard}>
+					<div class={styles.sectorImage}>
+						<img src={sector.image} alt="sector" />
+					</div>
+
+					<h3 class={styles.sectorTitle}>{sector.title}</h3>
+					<p class={styles.sectorDesc}>{sector.desc}</p>
+				</article>
+			{/each}
+		</div>
+
+		<div class={styles.ctaBtns}>
+			<Button onWhite primary>
+				<span>learn more</span>
+				<span class="material-symbols-sharp">expand_content</span>
+			</Button>
+
+			<Button onWhite>
+				<span>join us</span>
+				<span class="material-symbols-sharp">handshake</span>
+			</Button>
+		</div>
+	</section>
 </main>
