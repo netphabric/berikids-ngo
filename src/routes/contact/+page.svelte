bf3618f2ce1346c7e8ea3fc5b342b859af5dee83
sly <sylvester@archis.company>
2024-10-31 12:15:22 +0100

    extract contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,82 +1,24 @@
 <script lang>
-	import Button from "$lib/components/button/button.svelte"
+	import ContactForm from "$lib/components/contactForm/contactForm.svelte"
 	import Faq from "$lib/components/faq/faq.svelte"
 	import NewsletterForm from "$lib/components/newsletterForm/newsletterForm.svelte"
 	import PageSection from "$lib/components/pageSection/pageSection.svelte"
 	import SectionWrapper from "$lib/components/sectionWrapper/sectionWrapper.svelte"
 	import { contactFaqs } from "$lib/stores/faqStore"
-	import { addToast } from "$lib/stores/toastStore"
 	import styles from "./page.module.scss"
-
-	let name = ""
-	let email = ""
-	let message = ""
-
-	const handleSubmit = async () => {
-		const formData = { email, name, message }
-		if (!name || !email || !message) {
-			addToast("error", "Please fill in all fields")
-			return
-		}
-
-		const res = await fetch("/api/contact", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(formData)
-		})
-
-		if (res.ok) {
-			name = email = message = ""
-			addToast("success", "Message sent successfully")
-		} else {
-			addToast("error", "Failed to send message")
-		}
-	}
 </script>
 
 <main class={styles.root}>
 	<PageSection
 		isBlockLayout
 		chip="contact"
-		chipIcon="wifi_calling_bar_3"
 		title="Get in touch"
+		chipIcon="wifi_calling_bar_3"
 		desc="We’re here to answer your questions and explore ways to collaborate for a better future."
 	/>
 
 	<SectionWrapper className={styles.section}>
-		<form class={styles.sectionContent} on:submit|preventDefault={handleSubmit}>
-			<h4 class={styles.chip}>
-				<span class="material-symbols-sharp">outgoing_mail</span>
-				<span>connect</span>
-			</h4>
-
-			<h2 class={styles.title}>Reach out to us</h2>
-			<p class={styles.desc}>we'd love to hear from you</p>
-
-			<div class={styles.formGroup}>
-				<label for="name">
-					Name:
-					<input id="name" type="text" placeholder="Name" bind:value={name} />
-				</label>
-
-				<label for="email">
-					Email:
-					<input id="email" type="email" placeholder="email" bind:value={email} />
-				</label>
-
-				<label for="message">
-					Message:
-					<textarea id="message" placeholder="send us a message" bind:value={message} />
-				</label>
-			</div>
-
-			<div class={styles.ctaBtns}>
-				<Button onWhite secondary>
-					<span>submit</span>
-					<span class="material-symbols-sharp">send</span>
-				</Button>
-			</div>
-		</form>
+		<ContactForm />
 
 		<div class={styles.sectionImage}>
 			<img src="/images/cS1.webp" alt="berkids" />
